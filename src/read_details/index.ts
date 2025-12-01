8d142d0ffb5ba589cb65e258cd9a95262d4646a8
MustafaBilwani <mustafabilwani92@gmail.com>
2025-12-01 14:24:42 +0500

    remove duplicate, support excel convention

--- src/read_details/index.ts
+++ src/read_details/index.ts
@@ -1,17 +1,33 @@
 import { promises as fs } from "node:fs";
-import { read_pdf } from './read_pdf.js';
 import path from 'node:path';
 import { createWorker, type Worker } from "tesseract.js";
 import { glob } from "glob";
+import { getDocument } from 'pdfjs-dist/legacy/build/pdf.mjs';
+import type Canvas from "canvas";
+
+type Factory = {
+  canvas: Canvas.Canvas;
+};
+
+interface CanvasFactory {
+  create(
+    width: number,
+    height: number
+  ): Factory;
+};
 
 const workersQuantity = 3;
 
 let data = ``;
 
+const orderNumbers = new Set<string>();
+
+const prevDir = path.resolve('../');
+
 console.log(`using ${workersQuantity} workers`);
 console.time('create workers');
 
-export const workers = await Promise.all(
+const workers = await Promise.all(
   Array.from({ length: workersQuantity }, async () => ({
     worker: await createWorker('eng'),
     isfree: true
@@ -20,9 +36,36 @@ export const workers = await Promise.all(
 
 console.timeEnd('create workers');
 
-const prevDir = path.resolve('../');
+const addressRec = {
+  totalHeight: 1445,
+  totalWidth: 1020,
+  left: 449,
+  top: 936,
+  width: 517,
+  height: 271
+};
+const dateRec = {
+  totalHeight: 1445,
+  totalWidth: 1020,
+  left: 339,
+  top: 877,
+  width: 169,
+  height: 58
+};
+const orderRec = {
+  totalHeight: 1445,
+  totalWidth: 1020,
+  left: 496,
+  top: 809,
+  width: 291,
+  height: 61
+};
 
-main();
+console.time('Total time taken');
+
+await main();
+
+console.timeEnd('Total time taken');
 
 async function main() {
 
@@ -30,9 +73,8 @@ async function main() {
     cwd: prevDir,
     ignore: ['output/**', 'unrecognised/**'],
   });
-  console.log(files.length, 'files found');
 
-  console.time('Total time taken');
+  console.log(files.length, 'files found');
 
   let counter = 0;
 
@@ -44,7 +86,7 @@ async function main() {
 
         const pdf = files[counter++]!
         const pdfAbsolutePath = path.join(prevDir, pdf);
-        await processPdf(pdfAbsolutePath, worker);
+        await read_pdf(pdfAbsolutePath, worker);
 
       };
 
@@ -52,14 +94,12 @@ async function main() {
     })
   );
 
-  console.timeEnd('Total time taken');
-
   let c = 1;
   let filePath = `../output.csv`;
-  
+
   while (true) {
     try {
-     
+
       await fs.writeFile(filePath, data);
 
       console.log(`written to ${filePath}`);
@@ -71,20 +111,71 @@ async function main() {
   };
 };
 
-async function processPdf(pdfAbsolutePath: string, worker: Worker) {
+async function read_pdf(pdfPath: string, worker: Worker) {
 
-  // console.time(pdfAbsolutePath);
+  const pdfRawData = new Uint8Array(await fs.readFile(pdfPath));
 
-  const { length, getPage } = await read_pdf(pdfAbsolutePath, worker);
+  const pdfDocument = await getDocument({
+    data: pdfRawData,
+  }).promise;
 
-  for (let counter = 1; counter <= length; counter++) {
+  async function getPage(pageNumber: number) {
 
-    const row = await getPage(counter);
+    const page = await pdfDocument.getPage(pageNumber);
 
-    data += row + '\n';
+    const viewport = page.getViewport({ scale: 1.5 });
 
+    const { canvas } = (pdfDocument.canvasFactory as CanvasFactory).create(
+      viewport.width,
+      viewport.height
+    );
+
+    await page.render({
+      canvas,
+      viewport
+    }).promise;
+
+    const jpgBuffer = canvas.toBuffer("image/jpeg", {});
+
+    const { data: { text: order } } = await worker.recognize(jpgBuffer, {
+      rectangle: {
+        height: orderRec.height / orderRec.totalHeight * viewport.height,
+        width: orderRec.width / orderRec.totalWidth * viewport.width,
+        top: orderRec.top / orderRec.totalHeight * viewport.height,
+        left: orderRec.left / orderRec.totalWidth * viewport.width,
+      }
+    });
+
+    if (orderNumbers.has(order)) {
+      return;
+    }
+
+    orderNumbers.add(order);
+
+    const { data: { text: address } } = await worker.recognize(jpgBuffer, {
+      rectangle: {
+        height: addressRec.height / addressRec.totalHeight * viewport.height,
+        width: addressRec.width / addressRec.totalWidth * viewport.width,
+        top: addressRec.top / addressRec.totalHeight * viewport.height,
+        left: addressRec.left / addressRec.totalWidth * viewport.width,
+      }
+    });
+
+    const { data: { text: date } } = await worker.recognize(jpgBuffer, {
+      rectangle: {
+        height: dateRec.height / dateRec.totalHeight * viewport.height,
+        width: dateRec.width / dateRec.totalWidth * viewport.width,
+        top: dateRec.top / dateRec.totalHeight * viewport.height,
+        left: dateRec.left / dateRec.totalWidth * viewport.width,
+      }
+    });
+
+    data += `"${order}","${date}","${address}"\n` as const;
   };
 
-  // console.timeEnd(pdfAbsolutePath);
+  for (let counter = 1; counter <= pdfDocument.numPages; counter++) {
 
+    await getPage(counter);
+
+  };
 };
